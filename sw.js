c3ddb97068f4cfc5dbb9300009518265e774dc08
LyamovSergey <lyamofff@gmail.com>
2020-02-26 22:27:59 +0200

    add

--- sw.js
+++ sw.js
@@ -1,2 +1,11 @@
 'use strict';
-importScripts('sw-toolbox.js'); toolbox.precache(["index.html","dist/style.main.css","js/**.js"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+
+importScripts('sw-toolbox.js');
+
+toolbox.precache(["index.html","dist/style.main.css"]);
+
+toolbox.router.get('/images/*', toolbox.cacheFirst);
+
+toolbox.router.get('/*', toolbox.networkFirst, {
+  networkTimeoutSeconds: 5
+});
